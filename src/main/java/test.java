3a4b87dd274f43c7a50da4f7b6144088f7ef5ef4
512953708@qq.com <512953708@qq.com>
2019-07-04 11:46:20 +0800

    20190714HashMap 源码分析

---
+++ src/main/java/test.java
@@ -0,0 +1,9 @@
+public class test {
+
+    public static void main(String[] args) {
+        MyHashMap putVal=new MyHashMap();
+        putVal.put("1","1");
+        System.out.printf(putVal.size+"");
+
+    }
+}
